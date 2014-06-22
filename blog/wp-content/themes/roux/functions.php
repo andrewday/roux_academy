4713fade2ab582ed684df954ed584159e8aac505
Andrew Day <daya@andrewday.co.uk>
2014-06-22 12:26:47 +0100

    05/10 search text

--- blog/wp-content/themes/roux/functions.php
+++ blog/wp-content/themes/roux/functions.php
@@ -7,4 +7,11 @@ function siteRoot($theFolder) {
 	$thePath = substr($home, 0, $thePosition);
 	return $thePath;
 }
+
+function my_search_form($text) {
+     $text = str_replace('id="s"', 'id="s" placeholder="Enter keyword(s)"', $text);
+     $text = str_replace('value="Search"', 'value=""', $text);
+     return $text;
+}
+add_filter('get_search_form','my_search_form');
 ?>
