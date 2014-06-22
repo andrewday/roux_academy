5157b0a82742c29cd06d8b36c2b6655397af9fbc
Andrew Day <daya@andrewday.co.uk>
2014-06-22 10:50:29 +0100

    05/07 ManageCode

--- blog/wp-content/themes/roux/index.php
+++ blog/wp-content/themes/roux/index.php
@@ -6,17 +6,14 @@
             <?php while ( have_posts() ) : the_post(); ?>
                 <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
                     <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                    <div class="meta">
-                        <em>Posted on:</em> <?php the_time('F jS, Y') ?>
-                        <em>by</em> <?php the_author() ?>
-                    </div>
+                    
                     <div class="entry">
                         <?php the_content(); ?>
                     </div>
                     <div class="postmetadata">
-                        <?php the_tags('Tags: ', ', ', '<br />'); ?>
+    					  <?php comments_popup_link('No Comments ', '1 Comment', '% Comments'); ?> | 
                         Posted in <?php the_category(', ') ?> | 
-                        <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+                     
                     </div>
                 </article>
             <?php endwhile; ?>
